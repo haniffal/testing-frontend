b8837f9fd05e11eb216290c5c90d6799a7ead164
haniffal <04hanif10alfarisi02@gmail.com>
2024-12-11 18:14:38 +0700

    Update api.js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,8 +1,9 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = 'https://plant-disease-backend-957998016544.asia-southeast2.run.app';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
+  history: `${BASE_URL}/history`,
 };
 
 class PredictAPI {
@@ -16,4 +17,19 @@ class PredictAPI {
     const json = await response.json();
     return json;
   }
+
+  // Fungsi untuk menyimpan riwayat prediksi (opsional)
+  static async saveHistory(data) {
+    const response = await fetch(ENDPOINT.history, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const json = await response.json();
+    return json;
+  }
+
 }
